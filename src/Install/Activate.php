2f5ba093d74087655a9e0f8c9702739f3db099c4
Cyrus <david@jcyr.us>
2015-08-26 18:33:07 +0800

    update activation hook
    require atleast PHP version 5.4.0 and must be 64bit for the dropbox sdk to
    work.

--- src/Install/Activate.php
+++ src/Install/Activate.php
@@ -5,6 +5,8 @@ use MyWPBackup\Admin\Admin;
 
 class Activate {
 
+	const MIN_VER = '5.4.0';
+
 	public static function err( $message, $errno = E_USER_ERROR ) {
 
 		if ( isset( $_GET['action'] ) && 'error_scrape' === $_GET['action'] ) { // input var okay, sanitization okay
@@ -28,6 +30,14 @@ class Activate {
 		update_site_option( 'my-wp-backup-jobs', array() );
 		update_site_option( 'my-wp-backup-reporter', array() );
 		update_site_option( 'my-wp-backup-options', Admin::$options );
+
+		if ( version_compare( PHP_VERSION, self::MIN_VER, '<' ) ) {
+			self::err( sprintf( __( 'Your PHP version must be atleast %s for this plugin to function correctly. You have %s.', 'my-wp-backup' ), self::MIN_VER, PHP_VERSION ) );
+		}
+
+		if ( strlen( (string) PHP_INT_MAX ) < 19 ) {
+			self::err( __( 'This plugin requires the 64-bit version of PHP to function correctly.', 'my-wp-backup' ) );
+		}
 
 		$extensions = array( 'zlib', 'bz2', 'Phar', 'SPL', 'curl', 'mbstring' );
 
