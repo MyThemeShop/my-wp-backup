5aeffc03571107aa1687d0d0431727e8271c9022
Cyrus <david@jcyr.us>
2015-09-02 01:48:37 +0800

    do not delete options on deactivate

--- src/Install/Deactivate.php
+++ src/Install/Deactivate.php
@@ -4,10 +4,7 @@ namespace MyWPBackup\Install;
 class Deactivate {
 
 	public static function run() {
-
-		delete_option( 'my-wp-backup-jobs' );
-		delete_option( 'my-wp-backup-reporter' );
-		delete_option( 'my-wp-backup-options' );
+		
 		delete_transient( 'my-wp-backup-running' );
 		delete_transient( 'my-wp-backup-finished' );
 
